715faf3f0675c87723bb9953d59189b325c0cee6
Matt Moon <matthewjackmoon@gmail.com>
2022-12-21 22:06:19 -0700

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class",
+  content: ["./pages/**/*.{html,js}", "./components/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
