562a0aa78ff5cf432b7d3f742cae5c76e3f3d20c
Aagam Jain <aagamjain573@gmail.com>
2022-12-23 11:57:07 +0530

    user's photo bug fixup

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from "next/head";
 import { BsFillMoonStarsFill } from "react-icons/bs";
 import { AiFillGithub, AiFillLinkedin } from "react-icons/ai";
 import Image from "next/image";
-// import matt from "/public/IMG_3109.JPG";
+import matt from "/public/myphoto.png";
 import pokemonProject from "/public/Screenshot 2022-12-21 at 5.30.35 PM.png";
 import img1 from "/public/Screenshot 2022-12-21 at 5.32.07 PM.png";
 import img2 from "/public/Screenshot 2022-12-21 at 5.32.33 PM.png";
@@ -63,7 +63,7 @@ export default function Home() {
 
           <div className="relative mx-auto rounded-full w-80 h-80 overflow-hidden">
             <Image
-              src="/public/IMG_3109.JPG"
+              src={matt}
               className="rounded-full"
               layout="fill"
               objectFit="cover"
