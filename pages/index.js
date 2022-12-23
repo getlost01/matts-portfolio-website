213dabbdfdac36f7fcdd363bb87775f9234a000e
Aagam Jain <aagamjain573@gmail.com>
2022-12-23 11:40:41 +0530

    directory bug fixation

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,11 @@ import Head from "next/head";
 import { BsFillMoonStarsFill } from "react-icons/bs";
 import { AiFillGithub, AiFillLinkedin } from "react-icons/ai";
 import Image from "next/image";
-import matt from "/Users/mattmoon/Desktop/portfolio-site/public/IMG_3109.JPG";
-import pokemonProject from "/Users/mattmoon/Desktop/portfolio-site/public/Screenshot 2022-12-21 at 5.30.35 PM.png";
-import img1 from "/Users/mattmoon/Desktop/portfolio-site/public/Screenshot 2022-12-21 at 5.32.07 PM.png";
-import img2 from "/Users/mattmoon/Desktop/portfolio-site/public/Screenshot 2022-12-21 at 5.32.33 PM.png";
-import img3 from "/Users/mattmoon/Desktop/portfolio-site/public/Screenshot 2022-12-21 at 5.32.46 PM.png";
+import matt from "/public/IMG_3109.JPG";
+import pokemonProject from "/public/Screenshot 2022-12-21 at 5.30.35 PM.png";
+import img1 from "/public/Screenshot 2022-12-21 at 5.32.07 PM.png";
+import img2 from "/public/Screenshot 2022-12-21 at 5.32.33 PM.png";
+import img3 from "/public/Screenshot 2022-12-21 at 5.32.46 PM.png";
 import {
   DiJsBadge,
   DiHtml5,
@@ -17,7 +17,7 @@ import {
   DiGit,
 } from "react-icons/di";
 import { useState } from "react";
-import mLogo from "/Users/mattmoon/Desktop/portfolio-site/public/MM logo.png";
+import mLogo from "/public/MM logo.png";
 
 export default function Home() {
   const [darkMode, setDarkMode] = useState(false);
